6427e14ec7ab3c07d9fcd1f4ad8edd8bc793688b
Ademir Junior <ademir.jr.88.com>
2025-08-21 03:44:25 -0300

    atualização start2

--- start.sh
+++ start.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Aplica migrações
 python manage.py migrate
-python manage.py createsuperuser --noinput
+
+# Cria superusuário via script Python
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+import os
+
+User = get_user_model()
+username = os.getenv("DJANGO_SUPERUSER_USERNAME", "Ademir")
+email = os.getenv("DJANGO_SUPERUSER_EMAIL", "ademir.jr.88@hotmail.com")
+password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "Ademir22")
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+END
+
+# Inicia o servidor
 gunicorn setup.wsgi:application --bind 0.0.0.0:$PORT
