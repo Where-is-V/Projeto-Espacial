5b8c320ad5d945b402ceb757cf7ec40400d4f868
Ademir Junior <ademir.jr.88.com>
2025-08-21 03:36:55 -0300

    atualização start1

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 python manage.py migrate
-python manage.py shell < create_superuser.py
+python manage.py createsuperuser --noinput
 gunicorn setup.wsgi:application --bind 0.0.0.0:$PORT
